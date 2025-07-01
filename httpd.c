ecf97e054af094cbfef3bd0aaff8b024590db001
arjunrao <arjunkrao2004@gmail.com>
2025-07-01 10:12:26 +0530

    Accepts a new client connection and Loops to accept multiple clients, forks children

--- httpd.c
+++ httpd.c
@@ -3,7 +3,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
-#include <unisted.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -32,19 +32,55 @@ int srv_init(int portno){
   error="bind() error";
   return 0;
  }
+ if(listen(s,5))
+ {
+  close(s);
+  error="listen() error";
+  return 0;
+}
 
-
+//returns 0 on error , or return the new client's socket fd
+int cli_accept(int s){
+ int c;
+ socklen_t addrlen;
+ struct sockaddr_in cli;
+ 
+ addrlen=0;
+ memset(&cli,0,sizeof(cli));
+ c=accept(s,(struct socketaddr *)&cli, &addrlen);
+ if(c<0){
+  error="accept() error";
+  return 0;
+ }
+ return c;
 }
 
 int main(int argc,char *argv[]){
 
- int s;
+ int s,c;
  char *port;
  if(argc<2){
   fprintf(stderr,"Usage: %s <listening port>\n",argv[0]);
   return -1;
  }else 
    port = argv[1];
+ 
  s=srv_init(atoi(port));
-
+ if(!s){
+ fprintf(stderr,"%s\n",error);
+ return -1;
+ }
+ printf("Listening on %s:%c\n",LISTENADDR,port);
+ while(1){
+  c=cli_accept(s);
+  if(!c){
+   fprintf(stderr,"%s\n",error);continue;
+  }
+  printf("Incomming connetion\n");
+  
+  //for the main process return the new processes id , and for the new process return 0
+  if(!fork())
+   cli_conn(s,c); 
+ }
+ return -1;
 }
